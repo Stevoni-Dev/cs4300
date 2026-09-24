40a0c08a44428ab6d5f4fea086cefd612956b0ab
Stevoni-Dev <tovar.stephen@gmail.com>
2026-09-24 16:57:22 +0000

    Finished task 3.

---
+++ homework1/src/task3.py
@@ -0,0 +1,52 @@
+from math import sqrt
+
+# Check for negative, positive, or zero value of number
+def signum(number: int | float) -> str:
+    if not (isinstance(number, int) or isinstance(number, float)) or isinstance(number, bool):
+        print("Must provide integer or floating number")
+        return
+    if number < 0:
+        print("Number is negative")
+    elif number > 0: 
+        print("Number is positive")
+    else:
+        print("Number is zero")
+
+# Checks if a number is prime
+def is_prime(number: int) -> bool:
+    if not isinstance(number, int) or isinstance(number, bool):
+        print("Must provide integer")
+        return
+    # Based on algorithm, 1 will flag as prime but it is not
+    if number == 1:
+        return False
+        
+    # Get square root
+    square_root = sqrt(number)
+
+    # Check all numbers less than square root and see if it can be divided evenly
+    numbers_to_check = list(range(1, int(square_root) + 1))
+
+    for value in numbers_to_check:
+        if number % value == 0 and value != 1:
+            return False
+    return True
+
+def first_ten_prime():
+    prime_numbers = list()
+    number = 1
+    while len(prime_numbers) < 10:
+        if is_prime(number):
+            prime_numbers.append(number)
+        number += 1
+
+    for value in prime_numbers:
+        print(value)
+
+def triangle_of_100():
+    numbers = list(range(1, 101))
+    sum = 0
+    for number in numbers:
+        sum += number
+
+    return sum
---
+++ homework1/tests/test_task3.py
@@ -0,0 +1,59 @@
+import pytest
+from src.task3 import signum, is_prime, first_ten_prime, triangle_of_100
+
+signum_valid_positive_values = [*range(1,20), 0.1, 4.5, 15.5]
+signum_valid_negative_values = [*range(-1, -20), -0.9, -11.1]
+signum_valud_zero_values = [0.0, 0]
+signum_invalid_inputs = [True, False, "Orange", [1, 2, 3], {"a": 1}, None]
+
+@pytest.mark.parametrize("valid_positive_value", signum_valid_positive_values)
+def test_signum_positive(capsys, valid_positive_value):
+    signum(valid_positive_value)
+    captured = capsys.readouterr()
+    assert captured.out == "Number is positive\n"
+
+@pytest.mark.parametrize("valid_negative_value", signum_valid_negative_values)
+def test_signum_negative(capsys, valid_negative_value):
+    signum(valid_negative_value)
+    captured = capsys.readouterr()
+    assert captured.out == "Number is negative\n"
+
+@pytest.mark.parametrize("valid_zero_value", signum_valud_zero_values)
+def test_signum_zero(capsys, valid_zero_value):
+    signum(valid_zero_value)
+    captured = capsys.readouterr()
+    assert captured.out == "Number is zero\n"
+
+@pytest.mark.parametrize("invalid_value", signum_invalid_inputs)
+def test_signum_incorrect_input(capsys, invalid_value):
+    signum(invalid_value)
+    captured = capsys.readouterr()
+    assert captured.out == "Must provide integer or floating number\n"
+
+prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+non_prime_numbers = [item for item in range(1,31) if item not in prime_numbers]
+is_prime_invalid_inputs = [True, False, "Orange", [1, 2, 3], {"a": 1}, None, 3.14]
+
+print(non_prime_numbers)
+
+@pytest.mark.parametrize("prime_number", prime_numbers)
+def test_is_prime_with_prime_number(prime_number):
+    assert is_prime(prime_number) == True
+
+@pytest.mark.parametrize("non_prime_number", non_prime_numbers)
+def test_is_prime_with_non_prime_number(non_prime_number):
+    assert is_prime(non_prime_number) == False
+
+@pytest.mark.parametrize("invalid_input", is_prime_invalid_inputs)
+def test_is_prime_with_invalid_input(capsys, invalid_input):
+    is_prime(invalid_input)
+    captured = capsys.readouterr()
+    assert captured.out == "Must provide integer\n"
+
+def test_first_ten_prime(capsys):
+    first_ten_prime()
+    captured = capsys.readouterr()
+    assert captured.out == "2\n3\n5\n7\n11\n13\n17\n19\n23\n29\n"
+
+def test_triangle_of_100():
+    assert triangle_of_100() == 5050
